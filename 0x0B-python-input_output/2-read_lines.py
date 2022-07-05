ddd9e76217138d8aa7e48f3423cf0c67fab4c7b9
Braddon R <braddonr@gmail.com>
2022-07-05 21:23:25 +0300

    Third

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+'''  function that reads n lines of a text file (UTF8) and prints it to stdout
+'''
 
 
 def read_lines(filename="", nb_lines=0):
-    with open(filename, "r", encoding="UTF-8") as f:
-        if nb_lines <= 0:
-            print(f.read(), end="")
-        for index in range(nb_lines):
-            print(f.readline(), end="")
+    ''' function: read_lines
+    '''
+    if filename == "" or type(filename) != str:
+        return
+    if type(nb_lines) != int:
+        return
+    counter = 0
+    with open(filename, "r") as f:
+        for line in f:
+            counter += 1
+            if nb_lines <= 0 or (counter <= nb_lines and nb_lines > 0):
+                print(line, end='')
+            else:
+                break
